53aa4c3721cc702730506149640f0ebb3fc354b6
Thegretia <thomsp2001@gmail.com>
2025-09-07 00:20:54 +0200

    Update monthly_sales.sql

--- queries/monthly_sales.sql
+++ queries/monthly_sales.sql
@@ -1,1 +1,10 @@
-qwdwwd
+--Ventes totales par mois de toutes les transactions 
+
+SELECT 
+    DATE_FORMAT(trans_date, "%Y-%m) AS month,
+    COUNT (*) AS total_transactions,
+    SUM (amount) AS total_amount,
+FROM transactions
+GROUP BY DATE_FORMAT(trans_date, "%Y-%m")
+ORDER BY month;
+    
