4d25be85098916d8d3e22aa789a277ae6d4baa6e
Thegretia <thomsp2001@gmail.com>
2025-09-07 00:31:41 +0200

    Update approved_vs_declined.sql

--- queries/approved_vs_declined.sql
+++ queries/approved_vs_declined.sql
@@ -1,1 +1,12 @@
-
+--Ventes approuvees vs refusees par mois
+SELECT 
+     DATE_FORMAT(trans_date,"%Y-%m") AS month,
+     COUNT(*) AS total_transactions,
+     SUM(CASE WHEN state ="approved" THEN 1 ELSE 0 END) AS approved_count,
+     SUM(CASE WHEN state ="declined" THEN 1 ELSE 0 END) AS declined_count,
+     SUM(CASE WHEN state="approved" THEN amount ELSE 0 END) AS approved_amount,
+     SUM(CASE WHEN state ="declined" THEN amount ELSE 0 END) AS declined_amount
+     
+FROM transactions
+GROUP BY DATE_FORMAT(trans_date,"%Y-%m")
+ORDER BY month;
