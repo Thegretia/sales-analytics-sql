a9a1623438b474985699085cb8fb2feba317ab5d
Thegretia <thomsp2001@gmail.com>
2025-09-07 00:40:38 +0200

    Create sales_by_country.sql

---
+++ queries/sales_by_country.sql
@@ -0,0 +1,8 @@
+SELECT 
+country,
+COUNT(*) AS total_transactions,
+SUM(amount) AS total_amount,
+SUM(CASE WHEN state ="approved" THEN 1 ELSE 0 END) AS approved_amount
+FROM transactions
+GROUP BY country
+ORDER BY total_amount DESC;
