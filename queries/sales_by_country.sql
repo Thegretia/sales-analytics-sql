fb0ba7f064d5c1011429ee7d4877489baad53592
Thegretia <thomsp2001@gmail.com>
2025-09-07 01:00:18 +0200

    Update sales_by_country.sql

--- queries/sales_by_country.sql
+++ queries/sales_by_country.sql
@@ -1,8 +1,10 @@
+-- Ventes totales par pays
+-- Agregations par pays
 SELECT 
-country,
-COUNT(*) AS total_transactions,
-SUM(amount) AS total_amount,
-SUM(CASE WHEN state ="approved" THEN 1 ELSE 0 END) AS approved_amount
-FROM transactions
-GROUP BY country
-ORDER BY total_amount DESC;
+country,--Selectionne les pays
+COUNT(*) AS total_transactions,--Compte les transactions totales de chaque pays et les mets dans la colonne total_transactions
+SUM(amount) AS total_amount,-- Somme le montant total de ventes de chaques pays et les mets dans total_amount
+SUM(CASE WHEN state ="approved" THEN 1 ELSE 0 END) AS approved_amount-- Compte le nombre de transactions approved de chaque pays et les mets dans la colonne approved_amount
+FROM transactions --Depuis la table transactions
+GROUP BY country-- Regroupe par pays
+ORDER BY total_amount DESC;-- classez par montant total decroissant pour tous les pays
