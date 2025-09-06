de7d3bf33e59a17886aba12f9819be9e19d81c92
Thegretia <thomsp2001@gmail.com>
2025-09-07 01:14:37 +0200

    Update monthly_sales.sql

--- queries/monthly_sales.sql
+++ queries/monthly_sales.sql
@@ -1,10 +1,10 @@
 --Ventes totales par mois de toutes les transactions 
-
+--Requete temporelle(par mois)
 SELECT 
-    DATE_FORMAT(trans_date, "%Y-%m) AS month,
-    COUNT (*) AS total_transactions,
-    SUM (amount) AS total_amount,
-FROM transactions
-GROUP BY DATE_FORMAT(trans_date, "%Y-%m")
-ORDER BY month;
+    DATE_FORMAT(trans_date, "%Y-%m") AS month,--Selectionne l'annee et le mois des transactions effectue (d'ou le format "%Y-%m) et les mets dans la colonne month
+    COUNT(*) AS total_transactions,--Compte toutes les transactions pour chaque annee-mois et les mets dans la colonne total_transactions    
+    SUM(amount) AS total_amount-- Somme toutes les montants de toutes les transactions de chaque annee-mois et les mets dans la colonne total_amount
+FROM transactions--Depuis la table transactions
+GROUP BY DATE_FORMAT(trans_date, "%Y-%m")--Regroupe par annee-mois
+ORDER BY month;--Classe chronologiquement
     
