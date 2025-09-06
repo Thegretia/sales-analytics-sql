806d3e7f5624bf1006c4fe80fa922c5883f1fe0b
Thegretia <thomsp2001@gmail.com>
2025-09-07 00:53:27 +0200

    Update top_products.sql

--- queries/top_products.sql
+++ queries/top_products.sql
@@ -1,8 +1,11 @@
+-- Top produits par montant de ventes
+-- Agregation par produit
+
 SELECT
-product,
-COUNT(*) AS total_transactions,
-SUM(amount) AS total_amount,
-SUM(CASE WHEN state ="approved" THEN 1 ELSE 0 END) AS approved_amount
-FROM transactions
-GROUP BY product
-ORDER BY total_amount DESC;
+product, --selectionne le produit
+COUNT(*) AS total_transactions, --compte toutes les transactions de chaque produit et le met dans la colonne total_transactions
+SUM(amount) AS total_amount, --Somme le montant de total de chaque produit et le met dans la colonne total_amount 
+SUM(CASE WHEN state ="approved" THEN 1 ELSE 0 END) AS approved_amount --Somme le nombre de transactions approved et le met dans la colonne approved_amount
+FROM transactions --Depuis la table transactions
+GROUP BY product --Regroupe par produit
+ORDER BY total_amount DESC;-- classe par montant total decroissant
